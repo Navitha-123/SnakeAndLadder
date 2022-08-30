3ac8de10541984cf41dd12144af1ac3853d607a8
navitha <navithareddy112@gmail.com>
2022-08-30 22:13:18 +0530

    [navitha] 1.SnakeAndLadderPlayer

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("choose option 1.Position at 0 ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    SnakeAndLadderPlayer.PlayerPosition0();
+                    break;
+            }
+        }
+    }
+}
